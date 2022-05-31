3b821a071df172c6c7310290f7241265229ae2ba
Dmitrij Kunts <r206265@gmail.com>
2022-05-31 04:38:11 +0300

    bug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,10 @@ class HomeController extends Controller
 
     public function regenerateLink(User $user)
     {
-        $user->update(['home_page_link' => CreateHomePageLink::create()]);
+        $user->update([
+            'home_page_link' => CreateHomePageLink::create(),
+            'home_page_link_expire' => now()->addDays(7)
+        ]);
         return redirect(route('home_link', $user));
     }
 
